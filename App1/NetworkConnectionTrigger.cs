801fc1d8d1130616ebc71f00cf6bb56037e4f1a8
Taelon - Home <thebluenfurry1@hotmail.com>
2018-03-12 16:26:45 +1300

    update

---
+++ App1/NetworkConnectionTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace SplitView
+{
+    public class NetworkConnectionTrigger : StateTriggerBase
+    {
+        private bool requiresInternet;
+        public NetworkConnectionTrigger()
+        {
+            NetworkInformation.NetworkStatusChanged += NetworkInformationOnNetworkStatusChanged;
+        }
+        public bool RequiresInternet
+        {
+            get { return this.requiresInternet; }
+            set
+            {
+                var profile = NetworkInformation.GetInternetConnectionProfile();
+                if (profile != null && profile.GetNetworkConnectivityLevel() ==
+                NetworkConnectivityLevel.InternetAccess)
+                {
+                    SetActive(true);
+                }
+                else
+                {
+                    SetActive(false);
+                }
+            }
+        }
+
+        private async void NetworkInformationOnNetworkStatusChanged(object sender)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (NetworkInformation.GetInternetConnectionProfile() != null)
+                {
+                    SetActive(this.RequiresInternet);
+                }
+                else
+                {
+                    SetActive(!this.RequiresInternet);
+                }
+            });
+        }
+    }
+}
